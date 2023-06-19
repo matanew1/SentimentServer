9b50ea64d5bd568557237d76bc547d278606a610
matanew1 <matan.bardugo@s.afeka.ac.il>
2023-06-19 15:04:33 +0300

    added test reports

--- e2e/server.spec.js
+++ e2e/server.spec.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 import axios from 'axios';
+import fs from 'fs';
 
 test('Analysis text to sense', async ({ page }) => {
   const url = 'http://localhost:8080/sense';
@@ -10,7 +11,24 @@ test('Analysis text to sense', async ({ page }) => {
     headers: { 'Content-Type': 'application/json' }
   });
   expect(response.status).toBe(200);
-  const data = response.data;
-  expect(data).toBeInstanceOf(Object);
+  const data = response.data.sentiment;
+  expect(typeof data).toBe('string');
 
+  // Save test result
+  fs.writeFileSync('test-results/analysis-sense.json', JSON.stringify(response.data));
+});
+
+test('Analysis text to summary', async ({ page }) => {
+  const url = 'http://localhost:8080/summary';
+  const text = 'Superman';
+
+  const response = await axios.post(url, { text }, {
+    headers: { 'Content-Type': 'application/json' }
+  });
+  expect(response.status).toBe(200);
+  const data = response.data.text;
+  expect(typeof data).toBe('string');
+
+  // Save test result
+  fs.writeFileSync('test-results/analysis-summary.json', JSON.stringify(response.data));
 });
